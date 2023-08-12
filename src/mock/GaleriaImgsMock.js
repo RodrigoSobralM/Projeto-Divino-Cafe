99d1eac8b44767e1c709d321e1aff638be4e30bd
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2023-08-12 16:16:30 -0300

    ajustando mock das imgs da galeria

--- src/mock/GaleriaImgsMock.js
+++ src/mock/GaleriaImgsMock.js
@@ -1,94 +1,94 @@
 export const GALERIA_MOCK = [
     {
         id: 1,
-        imgPath: '../../src/img/galeria_1.jpg'
+        imgPath: '../../public/galeria_1.jpg'
     },
     {
         id: 2,
-        imgPath: '../../src/img/galeria_2.jpg'
+        imgPath: '../../public/galeria_2.jpg'
     },
     {
         id: 3,
-        imgPath: '../../src/img/galeria_3.jpg'
+        imgPath: '../../public/galeria_3.jpg'
     },
     {
         id: 4,
-        imgPath: '../../src/img/galeria_4.jpg'
+        imgPath: '../../public/galeria_4.jpg'
     },
     {
         id: 5,
-        imgPath: '../../src/img/galeria_5.jpg'
+        imgPath: '../../public/galeria_5.jpg'
     },
     {
         id: 6,
-        imgPath: '../../src/img/galeria_6.jpg'
+        imgPath: '../../public/galeria_6.jpg'
     },
     {
         id: 7,
-        imgPath: '../../src/img/galeria_7.jpg'
+        imgPath: '../../public/galeria_7.jpg'
     },
     {
         id: 8,
-        imgPath: '../../src/img/galeria_8.jpg'
+        imgPath: '../../public/galeria_8.jpg'
     },
     {
         id: 9,
-        imgPath: '../../src/img/galeria_9.jpg'
+        imgPath: '../../public/galeria_9.jpg'
     },
     {
         id: 10,
-        imgPath: '../../src/img/galeria_10.jpg'
+        imgPath: '../../public/galeria_10.jpg'
     },
     {
         id: 11,
-        imgPath: '../../src/img/galeria_11.jpg'
+        imgPath: '../../public/galeria_11.jpg'
     },
     {
         id: 12,
-        imgPath: '../../src/img/galeria_12.jpg'
+        imgPath: '../../public/galeria_12.jpg'
     },
     {
         id: 13,
-        imgPath: '../../src/img/galeria_13.jpg'
+        imgPath: '../../public/galeria_13.jpg'
     },
     {
         id: 14,
-        imgPath: '../../src/img/galeria_14.jpg'
+        imgPath: '../../public/galeria_14.jpg'
     },
     {
         id: 15,
-        imgPath: '../../src/img/galeria_15.jpg'
+        imgPath: '../../public/galeria_15.jpg'
     },
     {
         id: 16,
-        imgPath: '../../src/img/galeria_16.jpg'
+        imgPath: '../../public/galeria_16.jpg'
     },
     {
         id: 17,
-        imgPath: '../../src/img/galeria_17.jpg'
+        imgPath: '../../public/galeria_17.jpg'
     },
     {
         id: 18,
-        imgPath: '../../src/img/galeria_18.jpg'
+        imgPath: '../../public/galeria_18.jpg'
     },
     {
         id: 19,
-        imgPath: '../../src/img/galeria_19.jpg'
+        imgPath: '../../public/galeria_19.jpg'
     },
     {
         id: 20,
-        imgPath: '../../src/img/galeria_20.jpg'
+        imgPath: '../../public/galeria_20.jpg'
     },
     {
         id: 21,
-        imgPath: '../../src/img/galeria_21.jpg'
+        imgPath: '../../public/galeria_21.jpg'
     },
     {
         id: 22,
-        imgPath: '../../src/img/galeria_22.jpg'
+        imgPath: '../../public/galeria_22.jpg'
     },
     {
         id: 23,
-        imgPath: '../../src/img/galeria_23.jpg'
+        imgPath: '../../public/galeria_23.jpg'
     },
 ]
